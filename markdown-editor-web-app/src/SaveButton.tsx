b08c86471901fc1dc61fac21b1df35dddfa0f234
Pietrell <piotrplotast2006@gmail.com>
2024-04-04 18:31:42 +0200

    create: savebutton and styled it

---
+++ markdown-editor-web-app/src/SaveButton.tsx
@@ -0,0 +1,12 @@
+import iconSave from "./assets/icon-save.svg";
+
+export default function SaveButton() {
+  return (
+    <button className="bg-orange-200 flex items-center justify-center p-3 rounded space-x-2 sm:py-[0.547rem]">
+      <img src={iconSave} alt="save the file" className="w-4 h-4" />
+      <span className="hidden sm:inline sm:text-heading-m font-roboto text-white">
+        Save Changes
+      </span>
+    </button>
+  );
+}

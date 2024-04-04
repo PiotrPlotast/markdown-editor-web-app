de5eeb47fa0e9748570c8226af38412ad9ad3fa3
Pietrell <piotrplotast2006@gmail.com>
2024-04-04 20:25:16 +0200

    fix: fixed font

--- markdown-editor-web-app/src/ActiveDocName.tsx
+++ markdown-editor-web-app/src/ActiveDocName.tsx
@@ -2,12 +2,12 @@ import blank from "./assets/icon-document.svg";
 
 export default function ActiveDocName() {
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-4 sm:space-x-2">
       <div>
         <img src={blank} alt="" />
       </div>
       <div>
-        <p className="text-gray-500 text-body-s font-light font-roboto">
+        <p className="hidden sm:block text-gray-500 text-body-s font-light font-roboto">
           Document Name
         </p>
         <p className="text-white text-heading-m font-roboto">welcome.md</p>

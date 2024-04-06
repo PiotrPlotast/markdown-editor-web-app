cd7e40bc28ca02e05d3715428625b6ad270dd07f
Pietrell <piotrplotast2006@gmail.com>
2024-04-06 15:52:53 +0200

    fix: deleted wrong class

--- markdown-editor-web-app/src/SidebarToggle.tsx
+++ markdown-editor-web-app/src/SidebarToggle.tsx
@@ -6,7 +6,7 @@ export default function SidebarToggle({ sidebarIsOpen, setSidebarIsOpen }) {
       onClick={() => {
         setSidebarIsOpen(!sidebarIsOpen);
       }}
-      className="transition-all bg-gray-700 hover:bg-orange-200 w-14 h-14 flex items-center justify-center sm:h-[4.5rem] sm:w-[4.5rem] lg:hidden"
+      className="transition-all bg-gray-700 hover:bg-orange-200 w-14 h-14 flex items-center justify-center sm:h-[4.5rem] sm:w-[4.5rem]"
     >
       <img src={sidebarIsOpen ? iconClose : iconMenu} alt="" />
     </button>

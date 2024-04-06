90149eb4cf95cf605fd36838ed4a69fb09158238
Pietrell <piotrplotast2006@gmail.com>
2024-04-06 15:10:07 +0200

    create and style component

---
+++ markdown-editor-web-app/src/NewDocumentButton.tsx
@@ -0,0 +1,7 @@
+export default function NewDocumentButton() {
+  return (
+    <button className="transition-all bg-orange-200 hover:bg-orange-100 text-heading-m text-white px-11 py-3 rounded">
+      + New Document
+    </button>
+  );
+}

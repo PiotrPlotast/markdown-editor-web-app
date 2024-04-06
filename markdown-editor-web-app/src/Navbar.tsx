1c7ed40a20970e9e94c2c7c4bc344e53a5ac2242
Pietrell <piotrplotast2006@gmail.com>
2024-04-06 12:30:59 +0200

    update: add logo to navbar on bigger screens

--- markdown-editor-web-app/src/Navbar.tsx
+++ markdown-editor-web-app/src/Navbar.tsx
@@ -2,12 +2,17 @@ import SidebarToggle from "./SidebarToggle";
 import ActiveDocName from "./ActiveDocName";
 import DeleteButton from "./DeleteButton";
 import SaveButton from "./SaveButton";
+import logo from "./assets/logo.svg";
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 flex">
       <SidebarToggle />
       <div className="flex ml-6 py-2 sm:py-4 justify-between flex-grow">
-        <ActiveDocName />
+        <div className="flex items-center">
+          <img src={logo} alt="" className="hidden lg:block" />
+          <div className="hidden lg:block lg:h-full lg:w-[1px] lg:bg-gray-600 mr-6 lg:ml-8"></div>
+          <ActiveDocName />
+        </div>
         <div className="flex space-x-6 mr-2 sm:mr-4">
           <DeleteButton />
           <SaveButton />

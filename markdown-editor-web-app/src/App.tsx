faa243e857b8a8c3cbfd5d59ff2bcff971965d29
Pietrell <piotrplotast2006@gmail.com>
2024-04-06 15:55:34 +0200

    fix: remove unused class and div changed to empty tag

--- markdown-editor-web-app/src/App.tsx
+++ markdown-editor-web-app/src/App.tsx
@@ -4,13 +4,13 @@ import Sidebar from "./Sidebar";
 function App() {
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
   return (
-    <div className=" overflow-hidden">
+    <>
       <Navbar
         sidebarIsOpen={sidebarIsOpen}
         setSidebarIsOpen={setSidebarIsOpen}
       />
       <Sidebar sidebarIsOpen={sidebarIsOpen} />
-    </div>
+    </>
   );
 }
 

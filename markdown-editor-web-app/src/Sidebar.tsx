210b45f8c3ed337132dfede78ce0775e5aa66971
Pietrell <piotrplotast2006@gmail.com>
2024-04-06 17:24:01 +0200

    working on sidebar

--- markdown-editor-web-app/src/Sidebar.tsx
+++ markdown-editor-web-app/src/Sidebar.tsx
@@ -6,16 +6,18 @@ export default function Sidebar({ sidebarIsOpen }) {
     <div
       className={`${
         sidebarIsOpen ? `translate-x-0` : `-translate-x-full`
-      } w-64 transition-transform duration-200 ease-in-out bg-gray-900 fixed h-full z-50 `}
+      } flex flex-col justify-between  w-64 transition-transform duration-200 ease-in-out bg-gray-900 fixed h-full z-50 pl-6 pt-7`}
     >
-      <h1>
-        <img src={logo} alt="" />
-      </h1>
-      <h2 className="text-gray-500 text-heading-s uppercase font-roboto">
-        My Documents
-      </h2>
-      <NewDocumentButton />
-      <ul></ul>
+      <div>
+        <h1>
+          <img src={logo} alt="markdown editor logo" />
+        </h1>
+        <h2 className="text-gray-500 text-heading-s uppercase font-roboto my-7 tracking-[2px]">
+          My Documents
+        </h2>
+        <NewDocumentButton />
+        <ul></ul>
+      </div>
       <ThemeSwitcher />
     </div>
   );

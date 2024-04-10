d1f09d991c1d3e4b89bce5276f41ee418aa27130
Pietrell <piotrplotast2006@gmail.com>
2024-04-10 21:18:16 +0200

    update: add conditional rendering of document layouts

--- markdown-editor-web-app/src/App.tsx
+++ markdown-editor-web-app/src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import DocLayout from "./DocLayout";
+import NoDocLayout from "./NoDocLayout";
 function App() {
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [documentIsOpen, setDocumentIsOpen] = useState(true);
   return (
-    <>
+    <div className="h-[100dvh] overflow-hidden">
       <Navbar
         sidebarIsOpen={sidebarIsOpen}
         setSidebarIsOpen={setSidebarIsOpen}
       />
       <Sidebar sidebarIsOpen={sidebarIsOpen} />
-    </>
+      {documentIsOpen ? (
+        <DocLayout />
+      ) : (
+        <NoDocLayout sidebarIsOpen={sidebarIsOpen} />
+      )}
+    </div>
   );
 }
 

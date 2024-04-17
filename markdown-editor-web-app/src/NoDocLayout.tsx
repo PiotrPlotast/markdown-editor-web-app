24534db975319960cf842adad16388f9f86bce4f
Pietrell <piotrplotast2006@gmail.com>
2024-04-17 17:58:52 +0200

    add new document button if no docs exist

--- markdown-editor-web-app/src/NoDocLayout.tsx
+++ markdown-editor-web-app/src/NoDocLayout.tsx
@@ -1,3 +1,4 @@
+import NewDocumentButton from "./NewDocumentButton";
 export default function NoDocLayout({
   sidebarIsOpen,
 }: {
@@ -5,13 +6,14 @@ export default function NoDocLayout({
 }) {
   return (
     <div
-      className={` flex items-center justify-center h-full transition-all right-0 ${
+      className={` flex flex-col gap-4 items-center justify-center h-full transition-all right-0 ${
         sidebarIsOpen && "sm:ml-64"
       }`}
     >
       <span className="text-gray-600 font-roboto text-h4 sm:text-h2">
         No Document Selected
       </span>
+      <NewDocumentButton />
     </div>
   );
 }

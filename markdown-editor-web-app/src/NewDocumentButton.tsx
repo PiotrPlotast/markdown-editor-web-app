17017f557b54fe3924bc23db26bfb2f48e27ff8d
Pietrell <piotrplotast2006@gmail.com>
2024-04-21 11:08:45 +0200

    update: user can now add documents and store them in database

--- markdown-editor-web-app/src/NewDocumentButton.tsx
+++ markdown-editor-web-app/src/NewDocumentButton.tsx
@@ -1,6 +1,11 @@
+import { addDocument } from "./FireStore";
+
 export default function NewDocumentButton() {
   return (
-    <button className="transition-all bg-orange-200 hover:bg-orange-100 text-heading-m text-white px-11 py-3 rounded">
+    <button
+      onClick={addDocument}
+      className="transition-all bg-orange-200 hover:bg-orange-100 text-heading-m text-white px-11 py-3 rounded"
+    >
       + New Document
     </button>
   );

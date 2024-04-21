f3852cdad3ebea3e9ea44d3b329738ed5a2b8af9
Pietrell <piotrplotast2006@gmail.com>
2024-04-21 11:09:15 +0200

    update: preview works

--- markdown-editor-web-app/src/Previev.tsx
+++ markdown-editor-web-app/src/Previev.tsx
@@ -1,15 +1,22 @@
 import iconHidePreview from "./assets/icon-hide-preview.svg";
 import iconShowPreview from "./assets/icon-show-preview.svg";
+import Markdown from "react-markdown";
 export default function Previev({
   showPreview,
   setShowPreview,
   showEditor,
   setShowEditor,
+  markdown,
 }) {
+  const previevMarkdown = markdown;
   return (
-    <div className={`${!showPreview && "hidden"} sm:block`}>
+    <div
+      className={`${
+        !showPreview && "hidden"
+      } sm:block flex flex-col overflow-auto`}
+    >
       <div
-        className={` bg-gray-200 h-[2.625rem] flex items-center justify-between px-4`}
+        className={`bg-gray-200 h-[2.625rem] flex items-center justify-between px-4`}
       >
         <span className="text-gray-500 text-heading-s font-roboto tracking-[2px] font-medium">
           PREVIEV
@@ -23,8 +30,10 @@ export default function Previev({
           <img src={showEditor ? iconHidePreview : iconShowPreview} alt="" />
         </button>
       </div>
-      <div>
-        <input type="text" name="" id="" />
+      <div className="flex items-center justify-center ">
+        <div className="px-5 pt-4 font-robotoSlab max-w-2xl">
+          <Markdown>{previevMarkdown}</Markdown>
+        </div>
       </div>
     </div>
   );

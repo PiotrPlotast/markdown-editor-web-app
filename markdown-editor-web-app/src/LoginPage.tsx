66b2ebac57c9a323877b9f2560cdd5e7574f9561
Pietrell <piotrplotast2006@gmail.com>
2025-02-20 23:00:27 +0100

    feat: enhance LoginPage with GitHub authentication and improved styling

--- markdown-editor-web-app/src/LoginPage.tsx
+++ markdown-editor-web-app/src/LoginPage.tsx
@@ -1,20 +1,24 @@
+import { signInWithGithub } from "./supabase/auth";
+
 export default function LoginPage() {
   return (
-    <div className="bg-gray-700 text-gray-100 p-4 max-w-lg">
-      <h1>Login</h1>
-      <form>
-        <label>
-          Email:
-          <input type="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-      <h2>Or</h2>
-      <button>Login with Github</button>
+    <div className="h-dvh flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-100 p-8 rounded-lg">
+        <h1>Login</h1>
+        <form>
+          <label>
+            Email:
+            <input type="email" />
+          </label>
+          <label>
+            Password:
+            <input type="password" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+        <h2>Or</h2>
+        <button onClick={signInWithGithub}>Login with Github</button>
+      </div>
     </div>
   );
 }

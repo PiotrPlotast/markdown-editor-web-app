6fca30a7f6fc6c0cebabc03889559a4c2a77648e
Pietrell <piotrplotast2006@gmail.com>
2025-02-23 16:00:13 +0100

    start working on login with email

--- markdown-editor-web-app/src/LoginPage.tsx
+++ markdown-editor-web-app/src/LoginPage.tsx
@@ -1,18 +1,23 @@
-import { signInWithGithub } from "./supabase/auth";
-
+import { signInWithEmail, signInWithGithub } from "./supabase/auth";
+import { useState } from "react";
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="h-dvh flex items-center justify-center bg-gray-900">
       <div className="bg-gray-100 p-8 rounded-lg">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={() => signInWithEmail(email, password)}>
           <label>
             Email:
-            <input type="email" />
+            <input type="email" onChange={(e) => setEmail(e.target.value)} />
           </label>
           <label>
             Password:
-            <input type="password" />
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </label>
           <button type="submit">Login</button>
         </form>

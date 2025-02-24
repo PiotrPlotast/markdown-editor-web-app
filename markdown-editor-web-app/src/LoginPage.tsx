5343e0d8ec63fe057fb6278dd40c3ecbd1d6ae91
Pietrell <piotrplotast2006@gmail.com>
2025-02-24 19:23:11 +0100

    feat: add email registration functionality to LoginPage and update auth methods

--- markdown-editor-web-app/src/LoginPage.tsx
+++ markdown-editor-web-app/src/LoginPage.tsx
@@ -1,13 +1,25 @@
-import { signInWithEmail, signInWithGithub } from "./supabase/auth";
+import {
+  signInWithEmail,
+  signInWithGithub,
+  signUpNewUser,
+} from "./supabase/auth";
 import { useState } from "react";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  function handleEmailLogin(e: React.FormEvent) {
+    e.preventDefault();
+    signInWithEmail(email, password);
+  }
+  function handleEmailRegister(e: React.FormEvent) {
+    e.preventDefault();
+    signUpNewUser(email, password);
+  }
   return (
     <div className="h-dvh flex items-center justify-center bg-gray-900">
       <div className="bg-gray-100 p-8 rounded-lg">
         <h1>Login</h1>
-        <form onSubmit={() => signInWithEmail(email, password)}>
+        <form onSubmit={handleEmailLogin}>
           <label>
             Email:
             <input type="email" onChange={(e) => setEmail(e.target.value)} />
@@ -20,6 +32,20 @@ export default function LoginPage() {
             />
           </label>
           <button type="submit">Login</button>
+        </form>
+        <form onSubmit={handleEmailRegister}>
+          <label>
+            Email:
+            <input type="email" onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="submit">Register</button>
         </form>
         <h2>Or</h2>
         <button onClick={signInWithGithub}>Login with Github</button>
--- markdown-editor-web-app/src/supabase/auth.ts
+++ markdown-editor-web-app/src/supabase/auth.ts
@@ -4,9 +4,7 @@ async function signUpNewUser(email: string, password: string) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
-    options: {
-      emailRedirectTo: "https://example.com/welcome",
-    },
+    options: {},
   });
 }
 

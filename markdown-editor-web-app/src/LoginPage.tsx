3cb8118324602a7b31dbf49cf5ca17d7dae228ca
Pietrell <piotrplotast2006@gmail.com>
2025-02-24 20:28:49 +0100

    feat: enhance LoginPage with improved form styling and layout

--- markdown-editor-web-app/src/LoginPage.tsx
+++ markdown-editor-web-app/src/LoginPage.tsx
@@ -18,47 +18,69 @@ export default function LoginPage() {
   }
   return (
     <div className="h-dvh flex items-center justify-center bg-gray-900">
-      <div className="bg-gray-100 p-8 rounded-lg">
+      <div className="bg-gray-100 p-8 rounded-lg flex flex-col items-center">
         <h1>{login ? "Login" : "Register"}</h1>
         {login ? (
-          <form onSubmit={handleEmailLogin}>
+          <form onSubmit={handleEmailLogin} className="flex flex-col gap-4">
             <label>
               Email:
-              <input type="email" onChange={(e) => setEmail(e.target.value)} />
+              <input
+                className="w-full"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
             <label>
               Password:
               <input
+                className="w-full"
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </label>
-            <button type="submit">Login</button>
+            <button
+              type="submit"
+              className="text-gray-100 bg-orange-200 w-full p-2 rounded-lg hover:bg-orange-100 transition-all"
+            >
+              Login
+            </button>
           </form>
         ) : (
-          <form onSubmit={handleEmailRegister}>
+          <form onSubmit={handleEmailRegister} className="flex flex-col gap-4">
             <label>
               Email:
-              <input type="email" onChange={(e) => setEmail(e.target.value)} />
+              <input
+                className="w-full"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
             <label>
               Password:
               <input
+                className="w-full"
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </label>
-            <button type="submit">Register</button>
+            <button
+              className="text-gray-100 bg-orange-200 w-full p-2 rounded-lg hover:bg-orange-100 transition-all"
+              type="submit"
+            >
+              Register
+            </button>
           </form>
         )}
         <h2>
           {login ? "Do not have an account yet?" : "Already have an account?"}
         </h2>
-        <button onClick={() => setLogin(!login)}>
+        <button className="underline" onClick={() => setLogin(!login)}>
           {login ? "Register" : "Login"}
         </button>
         <h2>Or</h2>
-        <button onClick={signInWithGithub}>Login with Github</button>
+        <button className="underline" onClick={signInWithGithub}>
+          Login with Github
+        </button>
       </div>
     </div>
   );

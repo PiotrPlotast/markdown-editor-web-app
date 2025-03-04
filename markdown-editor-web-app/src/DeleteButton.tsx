ad1bb48bb10b0dd5ade092590fcaea2df41e0565
Pietrell <piotrplotast2006@gmail.com>
2025-03-04 13:18:47 +0100

    feat: enhance DeleteButton to manage document state after deletion

--- markdown-editor-web-app/src/DeleteButton.tsx
+++ markdown-editor-web-app/src/DeleteButton.tsx
@@ -2,11 +2,19 @@ import iconDelete from "./assets/icon-delete.svg";
 import { deleteDocument } from "./supabase/db";
 export default function DeleteButton({
   openedDocumentId,
+  documentsChanged,
+  setDocumentsChanged,
+  setOpenedDocumentId,
 }: {
   openedDocumentId: string;
+  documentsChanged: boolean;
+  setDocumentsChanged: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenedDocumentId: React.Dispatch<React.SetStateAction<string>>;
 }) {
   const handleDeleteDocument = async (documentId: string) => {
     await deleteDocument(documentId);
+    setOpenedDocumentId("");
+    setDocumentsChanged(!documentsChanged);
   };
   return (
     <button
